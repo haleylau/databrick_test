21575973c67bb83a029031003258fb661040db75
haleylau <haleylau>
2022-03-08 21:30:18 +0000

    creat nb

---
+++ census_data.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+# File location and type
+file_location = "/FileStore/tables/census_clean.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "false"
+first_row_is_header = "true"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+
+# COMMAND ----------
+
+df = df.drop("_c0")
+
+# COMMAND ----------
+
+# Create a view or table
+
+temp_table_name = "census_clean_csv"
+
+df.createOrReplaceTempView(temp_table_name)
+
+# COMMAND ----------
+
+df.describe()
+
+# COMMAND ----------
+
+
